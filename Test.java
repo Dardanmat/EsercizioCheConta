2edbc5de10129370657f3ca9115b7cedb2c5c352
Dardanmat <dardanmatias@gmail.com>
2020-12-15 11:01:48 +0100

    Scritto file che conta da 1 a 10

---
+++ Test.java
@@ -0,0 +1,18 @@
+
+package esercizi.contareda1a10;
+
+
+public class Test {
+    
+    public static void main(String[] args){
+        
+        for(int i = 0; i < 10; i++){
+            
+            System.out.println("Siamo al numero " + (i+1));
+            
+        }
+        
+    }
+    
+}
+
