9fbbd67ec9a23f1adfc7bdc56d774423f6e8bf95
Dardanmat <dardanmatias@gmail.com>
2020-12-15 11:02:40 +0100

    aggiunto commento

--- Test.java
+++ Test.java
@@ -6,9 +6,9 @@ public class Test {
     
     public static void main(String[] args){
         
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 10; i++){ //vai che conta
             
-            System.out.println("Siamo al numero " + (i+1));
+            System.out.println("Siamo al numero " + (i+1)); 
             
         }
         
