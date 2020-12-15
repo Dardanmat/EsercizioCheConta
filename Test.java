a2ebb85a07d6cde10632442887c15311999509a1
Dardanmat <dardanmatias@gmail.com>
2020-12-15 11:05:43 +0100

    Avevo sbagliato a fare l'add, ora dovrebbe essere cambiato il commento

--- Test.java
+++ Test.java
@@ -6,7 +6,7 @@ public class Test {
     
     public static void main(String[] args){
         
-        for(int i = 0; i < 10; i++){ //vai che conta
+        for(int i = 0; i < 10; i++){ //vai che conta da 1 a 10
             
             System.out.println("Siamo al numero " + (i+1)); 
             
